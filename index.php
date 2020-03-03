a43a37e9ca33c28c4a928e8ed96607fb90a63d3f
Flávio Ribeiro <flavio@odara.com.br>
2020-03-03 16:05:35 -0300

    {}

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
 use Codemy\Repositories\UserRepository as UserRepository;
-use Codemy\Filters\AuthFilter as AuthFilter;
+use Codemy\Filters\AuthFilter as AuthFilter;  
 use Codemy\DB\Sql as Sql;
+use Codemy\Models\Page as Page;
 require_once 'app/start.php';
 
 
 $app = new \Slim\Slim();
 
-$app->config('debug', true);
+$app->config('debug', true); 
 
 $app->get('/', function() {
 	
-	
-	$sql = new Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
+	//$sql = new Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
 
 });
 
 $app->run();
 
- ?>
+ ?> 
