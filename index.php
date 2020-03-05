9ffe5d2f55ecbadeeebe315b4bf94a7b8c626701
Flávio Ribeiro <flavio@odara.com.br>
2020-03-05 11:42:24 -0300

    {feature: add route and configure .htaccess / Admin ok}

--- index.php
+++ index.php
@@ -3,24 +3,36 @@
 use Codemy\Repositories\UserRepository as UserRepository;
 use Codemy\Filters\AuthFilter as AuthFilter;  
 use Codemy\DB\Sql as Sql;
-use Codemy\Models\Page as Page;
+use Slim\Slim;
+use Codemy\Models\Page;
+use Codemy\Models\PageAdmin;
 require_once 'app/start.php';
 
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true); 
 
-$app->get('/', function() {
+	$app->get('/', function() {
+		
+		$page = new Page();
+		$page->setTpl("index");
+		//$sql = new Sql();
+		//$results = $sql->select("SELECT * FROM tb_users");
+		//echo json_encode($results);
+	});
+	$app->get('/admin/', function() {
+		
 	
-	$page = new Page();
-	$page->setTpl("index");
-	//$sql = new Sql();
-	//$results = $sql->select("SELECT * FROM tb_users");
-	//echo json_encode($results);
-
-});
+		$page = new PageAdmin();
+		$page->setTpl("index");
+		//$sql = new Sql();
+		//$results = $sql->select("SELECT * FROM tb_users");
+		//echo json_encode($results);
+	});
 
 $app->run();
+
+
 
  ?> 
--- views-cache/index.e6f40c956211bfbda56f3b5b0be1c72b.rtpl.php
+++ views-cache/index.e6f40c956211bfbda56f3b5b0be1c72b.rtpl.php
@@ -1,3 +1,4 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
