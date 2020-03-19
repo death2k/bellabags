afa95d8b79b0e261f149e1cf6f72aa062ca442c1
Flávio Ribeiro <flavio@odara.com.br>
2020-03-19 08:35:46 -0300

    {feature ftcrud ok, correct bug  as }

--- index.php
+++ index.php
@@ -52,6 +52,40 @@ $app->config('debug', true);
 		exit;
 
 	});
+
+// CRUD CREATE UPDATE DELL - USERS
+	$app->get('/admin/users', function() {
+		User::verifyLogin();
+		$user = User::listAll();
+		$page = new PageAdmin();
+		$page->setTpl("users", array(
+			'user'=>$user
+		));
+		
+	});
+
+	$app->get('/admin/users/create', function() {
+		User::verifyLogin();
+		$page = new PageAdmin();
+		$page->setTpl("users-create");
+	});
+	$app->get('/admin/users/:iduser/delete ', function($iduser) {
+		User::verifyLogin();
+	});
+	$app->get('/admin/users/:iduser', function($iduser) {
+		User::verifyLogin();
+		$page = new PageAdmin();
+		$page->setTpl("users-update");
+	});
+	$app->post('/admin/users/create', function() {
+		User::verifyLogin();
+	});
+	$app->post('/admin/users/:iduser', function($iduser) {
+		User::verifyLogin();
+	});
+	
+
+
 $app->run();
 
 
